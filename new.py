1955df3238c9e8662270fd55fbe25bd377c2fb5d
Amal Pavithran <amalpavithranmp@gmail.com>
2019-12-16 13:38:45 +0530

    Changes

--- new.py
+++ new.py
@@ -1,24 +1,21 @@
 from dronekit import VehicleMode, connect
-import threading
-import time, logging
-import sys
+import threading, time, logging, sys
 
 logging.basicConfig(filename="debug.log",level=logging.DEBUG,format='%(asctime)s,%(msecs)d %(name)s %(message)s')   #LOG FILE FOR DEBUGGING
 logging.basicConfig(filename="error.log",level=logging.ERROR,format='%(asctime)s,%(msecs)d %(name)s %(message)s')   #LOG FILR FOR ERRORS
 
-#Connect to vehicles
-def connect_vehicle(vehicle_address,call_id):
+def connect_vehicle(vehicle_address):
     try:
         vehicle = connect(vehicle_address, wait_ready=True)
-        print("CONNECTED TO VEHICLE " + call_id)
-        logging.debug("CONNECTED TO VEHICLE " + call_id)
+        print("CONNECTED TO VEHICLE")
+        logging.debug("CONNECTED TO VEHICLE")
         return vehicle
     except:
-        print("CONNECTION FAILED")
+        print("Connection Failed")
         logging.error("CONNECTION FAILED")
         return 0
 
-def arm_and_takeoff(vehicle,height,call_id):
+def arm_and_takeoff(height):
     print("CHECKING IF ARMABLE")
     logging.debug("CHECKING IF ARMABLE")
 
@@ -30,15 +27,8 @@ def arm_and_takeoff(vehicle,height,call_id):
             print("TIMEOUT ERROR")
             logging.error("TIMEOUT ERROR")
             sys.exit()
-    print("VEHICLE ARMABLE" + (call_id))
-    logging.debug("VEHICLE ARMABLE" + (call_id))
+    print("VEHICLE ARMABLE")
+    logging.debug("VEHICLE ARMABLE")
 
 vehicle = connect_vehicle("/dev/ttyUSB0")
 
-while (vehicle.mode != VehicleMode("GUIDED")):
-    vehicle.mode = VehicleMode("GUIDED")
-    time.sleep(2)
-
-if __name__ == "__main__":
-    blinky = connect_vehicle('address','blinky')
-    buggy = connect_vehicle('address','buggy')
