c5fac8092f0d6cc0cf96fa18d089a1cf99afe8fb
Amal Pavithran <amalpavithranmp@gmail.com>
2020-01-02 15:58:49 +0530

    Fixed Exit Added JSON

--- client.py
+++ client.py
@@ -1,22 +1,28 @@
 import websockets,asyncio,threading,logging
 from dronekit import VehicleMode, connect
 import time,sys
+import json
+hello = {"name":"Amal","command":"1"}
 
 async def command_interface():
     uri = "ws://localhost:8765"
     while True:
         async with websockets.connect(uri) as websocket:
             command = input(">>")
-            await websocket.send(command)
+            hello['command'] = command
+            await websocket.send(json.dumps(hello))
             # print(f'>{command}')
 
             try:
                 greeting = await websocket.recv()
+                next = await websocket.recv()
             except:
-                print('ERROR')
+                print("Exception Raised: ",sys.exc_info()[0])
             print(f"<{greeting}")
+            print(f'<{next}')
             if(command=='exit'):
                 asyncio.get_event_loop().stop
+                sys.exit()
 # async def main():
 #     while(await command_interface()):
 #         continue
--- node.py
+++ node.py
@@ -1,7 +1,7 @@
 import websockets,asyncio,threading,logging
 from dronekit import VehicleMode, connect
 import time,sys
-
+import json
 logging.basicConfig(filename="debug.log",level=logging.DEBUG,format='%(asctime)s,%(msecs)d %(name)s %(message)s')   #LOG FILE FOR DEBUGGING
 logging.basicConfig(filename="error.log",level=logging.ERROR,format='%(asctime)s,%(msecs)d %(name)s %(message)s')   #LOG FILR FOR ERRORS
 
@@ -15,8 +15,12 @@ __________.____    .___ _______   ____  __._____.___.
         \/        \/           \/        \/ \/
 #####################################################
     [1] Connect
-    [2] 
+    [2] Arm & TakeOff
+    [3] Arm
 """
+#Command to override rc channel inputs
+#vehicle.channels.overrides = {'5':None, '6':None,'3':500}
+path = '/dev/ttyUSB0'
 
 def connect_vehicle(vehicle_address):
     try:
@@ -29,7 +33,7 @@ def connect_vehicle(vehicle_address):
         logging.error("CONNECTION FAILED")
         return False
 
-def arm_and_takeoff(vehicle,height):
+def arm(vehicle):
     print("CHECKING IF ARMABLE")
     logging.debug("CHECKING IF ARMABLE")
 
@@ -44,17 +48,26 @@ def arm_and_takeoff(vehicle,height):
     print("VEHICLE ARMABLE" + (call_id))
     logging.debug("VEHICLE ARMABLE" + (call_id))
 
+def arm_and_takeoff(vehicle,height):
+    arm(vehicle)
+    vehicle.is_armed = True
+
 async def command_get(websocket, path):
     command = await websocket.recv()
     print("Ready")
+    await websocket.send(f'{command}')
+    command = json.loads(f'{command}')['command']
     #Connect to Vehicle
     if f'{command}' == 'id':
         await websocket.send(f'< {call_id}')
-    elif 'start' in f'{command}':
-        path = f'{command}'.split(' ')[1]
-        print(path)        
-        await websocket.send(f'{path}')
-        vehicle = connect_vehicle(f'{path}')
+    elif 'start' in f'{command}' or f'{command}'=='1':
+        if(f'{command}'==1):
+            path = '/dev/ttyUSB0'
+        else:
+            path = f'{command}'.split(' ')[1]
+            print(path)
+            logging.debug(f'PATH SET TO: {path}')
+        vehicle = await connect_vehicle(f'{path}')
         if not vehicle:
             logging.error("CONNECTION FAILED")
             await websocket.send(f'CONNECTION FAILED')
